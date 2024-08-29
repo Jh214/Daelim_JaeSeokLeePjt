ecf1d4d7fdfdab86282b05c665eead24812c27c3
jh214 <sswed147@naver.com>
2024-08-29 14:05:13 +0900

    update : 메일 내용 디자인 변경

--- src/main/java/jaeseok/jaeseoklee/dto/user/SendEmailDto.java
+++ src/main/java/jaeseok/jaeseoklee/dto/user/SendEmailDto.java
@@ -35,20 +35,20 @@ public class SendEmailDto {
                 "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
                 "    <title>TeacHub Email Auth</title>\n" +
                 "</head>\n" +
-                "<body style=\"font-family: Arial, sans-serif; background-color: #1b1b1b; color: #cccccc; margin: 0; padding: 0;\">\n" +
-                "    <div style=\"width: 100%; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #2a2a2a; border-radius: 8px;\">\n" +
-                "        <div style=\"font-size: 24px; color: #ffffff; margin-bottom: 20px;\">\n" +
-                "            " + userId + " 님,\n" +
+                "<body style=\"font-family: Arial, sans-serif; background-color: #ffffff; color: #000000; margin: 0; padding: 0;\">\n" +
+                "    <div style=\"width: 100%; max-width: 600px; margin: 0 auto; padding: 20px;/* background-color: rgb(230, 230, 248);*/ border-radius: 8px; /*border: 1px solid rgb(230, 230, 248);*/\">\n" +
+                "        <div style=\"font-size: 24px; color: #000000; margin-bottom: 20px; background-color: rgb(230, 230, 260); padding: 10px\">\n" +
+                "            " + userId + " 님\n" +
                 "        </div>\n" +
-                "        <div style=\"background-color: #2a2a2a; padding: 20px; border-radius: 8px;\">\n" +
-                "            <p style=\"color: #ffffff;\">새로운 비밀번호로 변경하려면 <span style=\"color: #f0c674;\"></span> 코드가 필요합니다.</p>\n" +
+                "        <div style=\"padding: 20px; border-radius: 8px; text-align: center;\">\n" +
+                "            <p style=\"color: #000000;\">새로운 비밀번호로 변경하려면 <span style=\"color: #000000;\"></span> 인증코드가 필요합니다.</p>\n" +
                 "            <div style=\"text-align: center; margin-bottom: 10px;\">\n" +
                 "            </div>\n" +
-                "            <div style=\"font-size: 36px; color: #60aee5; text-align: center; margin: 20px 0; font-weight: bold;\">\n" +
-                "                " + ranCode + "\n" +
+                "            <div style=\"font-size: 36px; color: #000000; text-align: center; margin: 20px 0; font-weight: bold; border-top: 1.5px solid rgb(230, 230, 248); border-bottom: 1.5px solid rgb(230, 230, 248); padding: 50px\">\n" +
+                "                인증번호 : " + ranCode + "\n" +
                 "            </div>\n" +
-                "            <p style=\"color: #ffffff;\">\n" +
-                "                페이지로 돌아가서 위 코드를 입력하여 인증하세요.<br>\n" +
+                "            <p style=\"color: #000000;\">\n" +
+                "                페이지로 돌아가서 위 코드를 입력하여 인증해주세요.<br>\n" +
                 "            </p>\n" +
                 "        </div>\n" +
                 "    </div>\n" +
